2d782f37c114588db4e5e8dcf21599600e4b2e4b
AdrianDevid <adriandevid36@gmail.com>
2022-11-28 10:35:51 -0300

    first commit

---
+++ ConversorQuery/EntitiesMapping/TipoFase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorQuery.EntitiesMapping
+{
+    public class TipoFase
+    {
+        public long cd_tp_fase_pk { get; set; }
+        public string nm_fase { get; set; }
+    }
+}
